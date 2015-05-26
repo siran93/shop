47c555cd23c53bf7b25addcb973ad34807f69e5a
siranusha93 <siranush.araqelyan@gmail.com>
2015-05-26 13:01:28 +0400

    footer test

--- application/views/footer.php
+++ application/views/footer.php
@@ -10,12 +10,12 @@
 		<?php
 			// $isAdmin = '';
 			// if($isAdmin == 'true') { ?>
-				<script src="<?=base_url()?>/application/libraries/DataTables/media/js/jquery.dataTables.js" type="text/javascript" charset="utf8"></script>
-				<script src="<?=base_url()?>assets/code/scripts/flot/jquery.flot.js" type="text/javascript"></script>
-			 	<script src="<?=base_url()?>assets/code/scripts/flot/jquery.flot.resize.js" type="text/javascript"></script>
-			    <script src="<?=base_url()?>assets/code/scripts/common.js" type="text/javascript"></script>
-				<script src='<?=base_url()?>assets/js/scrolling.js'></script>
-				<script src='<?=base_url()?>assets/js/admin.js' type="text/javascript"></script>
+			<script src="<?=base_url()?>/application/libraries/DataTables/media/js/jquery.dataTables.js" type="text/javascript" charset="utf8"></script>
+			<script src="<?=base_url()?>assets/code/scripts/flot/jquery.flot.js" type="text/javascript"></script>
+		 	<script src="<?=base_url()?>assets/code/scripts/flot/jquery.flot.resize.js" type="text/javascript"></script>
+		    <script src="<?=base_url()?>assets/code/scripts/common.js" type="text/javascript"></script>
+			<script src='<?=base_url()?>assets/js/scrolling.js'></script>
+			<script src='<?=base_url()?>assets/js/admin.js' type="text/javascript"></script>
 		<?php //} ?>
 		<script src='<?=base_url()?>assets/js/script.js'></script>
     	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=false"></script>
